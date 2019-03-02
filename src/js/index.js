4b41b8b1deeb1b78185ff2ce42b649bc57607ca6
Loweski <lengvang9000@gmail.com>
2019-03-02 14:24:19 -0800

    added search functionality and render data results

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 // dd5e645bbb83f801cbd3d48abcd7926b
 // https://www.food2fork.com/api/search
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 /*Global State of the app
   -Seach object
@@ -13,22 +15,25 @@ const state = {};
 
 const controlSearch = async () => {
   // 1. Get Query from view
-  const query = 'pizza' //// TODO: Replace placeholder data for actual query
+  const query = searchView.getInput();
+  console.log(query);
   if(query) {
     // 2. New search object and add to state
     state.search = new Search(query);
 
     // 3. Prepare UI for results (loading spinner)
+    searchView.clearInput();
+    searchView.clearResults();
 
     // 4. Search for recipes
     await state.search.getResults();
 
     // 5. Render results on UI (only after we get results)
-    console.log(state.search.result); // TODO: Replace with UI method.
+    searchView.renderResults(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e=> {
+elements.searchForm.addEventListener('submit', e=> {
   e.preventDefault();
   controlSearch();
 });
