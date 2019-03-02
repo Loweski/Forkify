69e99c0fe991ba08f3905db901b0b02fab800f91
Loweski <lengvang9000@gmail.com>
2019-03-01 21:01:09 -0800

    added model for search feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+// dd5e645bbb83f801cbd3d48abcd7926b
+// https://www.food2fork.com/api/search
+import Search from './models/Search';
+
+/*Global State of the app
+  -Seach object
+  -Current recipe object
+  -Shopping list object
+  -Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Get Query from view
+  const query = 'pizza' //// TODO: Replace placeholder data for actual query
+  if(query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results (loading spinner)
+
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI (only after we get results)
+    console.log(state.search.result); // TODO: Replace with UI method.
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e=> {
+  e.preventDefault();
+  controlSearch();
+});
